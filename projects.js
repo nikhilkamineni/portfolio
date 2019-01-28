7e3a803862ab0b2e54f5df8ae2862264fe8ef3e2
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2019-01-28 14:48:24 -0800

    Add zen-gram to projects

--- projects.js
+++ projects.js
@@ -2,25 +2,36 @@ export default [
   {
     name: 'MapScratcher',
     githubRepo: 'MapScratcher',
-    description: 'virtual "scratch off" map. features a custom world map built with Leaflet',
+    description:
+      'virtual "scratch off" map. features a custom world map built with open source libraries',
     deploymentLink: 'https://www.mapscratcher.app',
   },
   {
     name: '{ codex }',
     githubRepo: 'codex',
-    description: 'a rolodex from code snippets. features a built-in code editor with syntax highlighting and more',
+    description:
+      'a rolodex from code snippets. features a built-in code editor with syntax highlighting and more',
     deploymentLink: 'https://codex-notes.herokuapp.com',
+  },
+  {
+    name: 'zen-gram',
+    githubRepo: 'zen-gram',
+    description:
+      'a web app that provides a distraction and addiction-free portal to instagram. features webscraping with Cheerio.js',
+    deploymentLink: 'https://zen-gram.herokuapp.com',
   },
   {
     name: 'nebulous_groove',
     githubRepo: 'nebulous-groove',
-    description: 'a random groove generator with step sequencer and effects. all sounds are synthesized in real-time using the Web Audio API',
+    description:
+      'a random groove generator with step sequencer and effects. all sounds are synthesized in real-time using the Web Audio API',
     deploymentLink: 'https://nebulous-groove.now.sh',
   },
   {
     name: 'git-herd',
     githubRepo: 'git-herd',
-    description: 'super simple command line tool written in python. clones forked repos from multiple users',
-    deploymentLink: 'https://github.com/nikhilkamineni/git-herd'
-  }
+    description:
+      'super simple command line tool written in python. clones forked repos from multiple users',
+    deploymentLink: 'https://github.com/nikhilkamineni/git-herd',
+  },
 ]
